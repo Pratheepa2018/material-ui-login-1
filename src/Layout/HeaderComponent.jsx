fd2609b492a819fc7643d1152ef19df7c156b92c
Chetan HV <chetan.hv@collabera.com>
2020-06-24 14:54:25 +0530

    tabs submenu added

--- src/Layout/HeaderComponent.jsx
+++ src/Layout/HeaderComponent.jsx
@@ -3,13 +3,14 @@ import {
   AppBar,
   Toolbar,
   IconButton,
-  Button, Tabs, Tab, Box,
+  Button, Tabs, Tab, Box,Popover, MenuItem, Link
 } from '@material-ui/core';
 import {NotificationContainer} from 'react-notifications';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Breadcrumbs from './BreadCrumbs';
 import 'react-notifications/lib/notifications.css';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 const styles = theme => ({
   grow: {
     flexGrow: 1,
@@ -26,8 +27,37 @@ const styles = theme => ({
   },
 });
 class Header extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: "One",
+      label: "Three",
+      content: "One",
+      anchorEl: null
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClick(event) {
+    event.stopPropagation();
+    this.setState({
+      anchorEl: event.currentTarget
+    });
+  }
+
+
+  handleClose() {
+    this.setState({
+      anchorEl: null
+    });
+  }
+
   render() {
     const { classes } = this.props;
+    const open = Boolean(this.state.anchorEl);
     return (
     <div>
       <AppBar position="sticky" >
@@ -43,12 +73,40 @@ class Header extends React.Component {
       <Toolbar className={classes.appSubBar} >
       <Tabs value={'/login'}>
         <Tab value ={'/login'} color="inherit" label="Home" />
-        <Tab value ={'/solutions'} color="inherit" label="Solutions" />
+        <Tab value ={'/solutions'} color="inherit" label="Solutions"  onClick={this.handleClick} />
         <Tab value ={'/support'} color="inherit" label="Support" />
         <Tab value ={'/devlopers'} color="inherit" label="Devlopers" />
         <Tab value ={'/about'} color="inherit" label="About" />
         <Tab value = {'/contact'} color="inherit" label="Contact us" />
       </Tabs>
+      <Popover
+          open={open}
+          anchorEl={this.state.anchorEl}
+          onClose={this.handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center"
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center"
+          }}
+        >
+          <MenuItem>
+            Collabera Information Platform (CIP)
+          </MenuItem>
+          <MenuItem >
+          <Link href="/collaberadevopsplatform" >
+          Collabera DevOps Platform (CDP) <ArrowRightIcon />
+          </Link>
+          </MenuItem>
+          <MenuItem >
+            Collabera Connectors MarketPlace (CCM)
+          </MenuItem>
+          <MenuItem >
+            Collabera Cloud Accelerators (CCA)
+          </MenuItem>
+        </Popover>
       
       </Toolbar>
     </AppBar>
