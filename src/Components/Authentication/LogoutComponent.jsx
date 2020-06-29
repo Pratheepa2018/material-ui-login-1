6c4e4a463e78ee4bad99f3ce1a8d1d9abb651977
Jishnuparakkat <jishnuparakkate@gmail.com>
2020-06-29 09:53:38 +0530

    Remove unwanted library call from logout component

--- src/Components/Authentication/LogoutComponent.jsx
+++ src/Components/Authentication/LogoutComponent.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Button} from '@material-ui/core';
-import { useHistory } from 'react-router-dom';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-// import LoginComponent from '../Login/LoginComponent';
 
 export default function Logout() {
-  const history = useHistory();
   function handleLogout() {
     localStorage.removeItem("token");
-    // history.push('/login');
     window.location.reload(false)
   }
   return(
