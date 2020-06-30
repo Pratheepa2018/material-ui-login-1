8a336100bf952dcf92ab7b36688a3b7ad062be25
Jishnuparakkat <jishnuparakkate@gmail.com>
2020-06-30 12:18:53 +0530

    Change the table structure

--- src/Components/Connectors/ConnectorsTableComponent.jsx
+++ src/Components/Connectors/ConnectorsTableComponent.jsx
@@ -336,7 +336,7 @@ export default function EnhancedTable(props) {
                   {stableSort(rows, getComparator(order, orderBy))
                     .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                     .map((row, index) => {
-                      const isItemSelected = isSelected(row.name);
+                      const isItemSelected = isSelected(row.connector_name);
                       const labelId = `enhanced-table-checkbox-${index}`;
 
                       return (
@@ -346,16 +346,16 @@ export default function EnhancedTable(props) {
                           role="checkbox"
                           aria-checked={isItemSelected}
                           tabIndex={-1}
-                          key={row.name}
+                          key={row.connector_name}
                           selected={isItemSelected}
                         >
 
                           <TableCell padding="checkbox">
-                            <Checkbox
+                            {/* <Checkbox
                               checked={isItemSelected}
-                              onClick={(event) => handleClick(event, row.name)}
+                              onClick={(event) => handleClick(event, row.connector_name)}
                               inputProps={{ 'aria-labelledby': labelId }}
-                            />
+                            /> */}
                           </TableCell>
                           <TableCell component="th" id={labelId} scope="row" padding="none" >
                             {row.connector_name}
@@ -365,8 +365,11 @@ export default function EnhancedTable(props) {
                           <TableCell align="right">
                             <Button aria-label="edit" variant="outlined" color="primary" onClick={(event) => handleEdit(event, row.connectorId)}>
                               <Edit />
-                              <span> Edit </span>
                             </Button>
+                            <Button aria-label="delete" variant="outlined" color="primary">
+                              <DeleteForever />
+                            </Button>
+                            
                           </TableCell>
                         </TableRow>
                       );
