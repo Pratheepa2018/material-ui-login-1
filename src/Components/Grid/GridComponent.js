f2dc30c38ef9e6e29e26f316d727341f366fd266
Jishnuparakkat <jishnuparakkate@gmail.com>
2020-07-01 17:04:50 +0530

    Add loader and UI refactor dashboard page

--- src/Components/Grid/GridComponent.js
+++ src/Components/Grid/GridComponent.js
@@ -1,50 +1,50 @@
 import React, { Component } from 'react';
-import { Grid, Card, CardContent ,Typography, CardActions, Button } from '@material-ui/core/';
+import { Grid, Card, CardContent, Typography, CardActions, Button } from '@material-ui/core/';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import './GridComponent.css';
 
 
 class GridComponent extends Component {
-    render() {
-        const {card, gridSize, textAlign}=this.props;
-        return (
-            <Grid item key={card.id} xs={12} sm={6} md={gridSize ? gridSize : 4} alignItems="center" justify="center">
-                            <Card className={`grid_card ${textAlign}`}  >
-                                <img src={card.image} alt={card.imageText} className='grid_img' />
-                                <CardContent className='grid_content'>
-                                    <Typography gutterBottom variant="h5" component="h2">
-                                        {card.title}
-                                </Typography>
-                                    <Typography>
-                                       {card.discription}
-                                </Typography>
-                                </CardContent>
-                                <CardActions>
-                                    {card.learnMorelink && (
-                                <Button
-                                        variant="contained"
-                                        color="primary"
-                                        endIcon={<KeyboardArrowRightIcon>send</KeyboardArrowRightIcon>}
-                                        href={card.learnMorelink}
-                                    >
-                                        Learn More
-                                    </Button>
-                                    )}
-                                    <Button
-                                        variant="contained"
-                                        color="primary"
-                                        className='right_btn'
-                                        href={card.letsGolink}
-                                        endIcon={<KeyboardArrowRightIcon>Do Automation</KeyboardArrowRightIcon>}
-                                    >
-                                        Do Automation
-                                    </Button>
-                                </CardActions>
-                            </Card>
-                        </Grid>
-                  
-        );
-    }
+  render() {
+    const { card, gridSize, textAlign } = this.props;
+    return (
+      <Grid item key={card.id} xs={12} sm={6} md={gridSize ? gridSize : 4} alignItems="center" justify="center" className="dashboard-grid-container">
+        <Card className={`grid_card ${textAlign}`}  >
+          <figure className="box-figure">
+            <img src={card.image} alt={card.imageText} className='grid_img' />
+          </figure>
+          <CardContent className='grid_content'>
+            <Typography gutterBottom variant="h5" component="h2">
+              {card.title}
+            </Typography>
+            <Typography>
+              {card.discription}
+            </Typography>
+          </CardContent>
+          <CardActions className="dashboard-grid-action">
+            {card.learnMorelink && (
+              <Button
+                variant="outlined"
+                color="primary"
+                href={card.learnMorelink}
+              >
+                Learn More
+              </Button>
+            )}
+            <Button
+              variant="contained"
+              color="primary"
+              className='right_btn'
+              href={card.letsGolink}
+            >
+              Do Automation
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+
+    );
+  }
 }
 
 export default GridComponent;
---
+++ src/Layout/Loader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/nj-loader.scss';
+export const PageLoader = () => {
+  return(
+    <div className="page-loader">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  )
+}
