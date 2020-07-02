7da0371f9736f912f8a656b1db05906a91847446
Chetan HV <chetan.hv@collabera.com>
2020-07-02 11:56:28 +0530

    description spell changes

--- src/Components/SubscribedServices/SubscribedServicesComponent.js
+++ src/Components/SubscribedServices/SubscribedServicesComponent.js
@@ -5,10 +5,10 @@ import FullWidthBanner from '../FullWidthBanner/FullWidthBanner'
 import GridComponent from '../Grid/GridComponent'
 
 const cards = [
-    { id: 1, title: 'Collabera Information Platform(CIP)', discription: 'discription not available', image: 'https://www.m-files.com/blog/wp-content/uploads/2018/05/Img-IMMovingToTheCloudAndSaaS-600x300.jpg', learnMorelink: '#', letsGolink: '' },
-    { id: 2, title: 'Collabera DevOps Platform(CDP)', discription: 'discription not available', image: 'https://quantummile.com/wp-content/uploads/2018/03/DevOps-FAQ-1280x720-600x300.jpg', learnMorelink: '#', letsGolink: './subscribedservices/CDP' },
-    { id: 3, title: 'Collabera Connectors MarketPlace(CCM)', discription: 'discription not available', image: 'https://www.mediamath.com/blog/wp-content/uploads/2017/05/HitTheTarget-600x300.png', learnMorelink: '#', letsGolink: '' },
-    { id: 4, title: 'Collabera Cloud Accelerators(CCA)', discription: 'discription not available', image: 'https://www.torontointernetcity.com/wp-content/uploads/2018/09/cloud_computing--600x300.jpg', learnMorelink: '#', letsGolink: '' },
+    { id: 1, title: 'Collabera Information Platform(CIP)', description: '', image: 'https://www.m-files.com/blog/wp-content/uploads/2018/05/Img-IMMovingToTheCloudAndSaaS-600x300.jpg', learnMorelink: '#', letsGolink: '' },
+    { id: 2, title: 'Collabera DevOps Platform(CDP)', description: '', image: 'https://quantummile.com/wp-content/uploads/2018/03/DevOps-FAQ-1280x720-600x300.jpg', learnMorelink: '#', letsGolink: './subscribedservices/CDP' },
+    { id: 3, title: 'Collabera Connectors MarketPlace(CCM)', description: '', image: 'https://www.mediamath.com/blog/wp-content/uploads/2017/05/HitTheTarget-600x300.png', learnMorelink: '#', letsGolink: '' },
+    { id: 4, title: 'Collabera Cloud Accelerators(CCA)', description: '', image: 'https://www.torontointernetcity.com/wp-content/uploads/2018/09/cloud_computing--600x300.jpg', learnMorelink: '#', letsGolink: '' },
 ]
 
 class SubscribedServicesComponent extends Component {
