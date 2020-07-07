dca35c0bb0f370a85165d429e38924c7087ddbdb
Jishnuparakkat <jishnuparakkate@gmail.com>
2020-07-07 16:50:18 +0530

    Style the breadcrumbs and page banner

--- src/Components/FullWidthBanner/FullWidthBanner.js
+++ src/Components/FullWidthBanner/FullWidthBanner.js
@@ -2,44 +2,26 @@ import React from 'react';
 import Box from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import './FullWidthBanner.css';
+import '../../Styles/banner.scss';
 
 import Breadcrumbs from '../../Layout/BreadCrumbs';
 
 class FullWidthBanner extends React.Component {
-    
-    render() {
-        const { image, imageText, title, exceptimage}= this.props; //description, linkText
-        return (
-          <div>
+
+  render() {
+    const { image, imageText, title, exceptimage } = this.props; //description, linkText
+    return (
+      <div>
         <Box className='banner_body' style={{ backgroundImage: `url(${image})` }} p={{ xs: 4, sm: 6, md: 8 }}>
-     
-      {<img style={{ display: 'none' }} src={image} alt={imageText} />}
-      <div className='overlayer' />
-      <Grid container>
-        <Grid item md={6}>
-          <div className='textbody'>
-         
-            <Typography component="h1" variant="h3" color="inherit" gutterBottom>
-              {title}
-            </Typography>
-            {/* <Typography variant="h5" color="inherit" paragraph>
-              {description}
-            </Typography>
-            <Link variant="subtitle1" href="#">
-              {linkText}
-            </Link> */}
-          </div>
-          <div className='gif'>
-            <img  src={exceptimage} alt= 'Except more'/>
-          </div>
-        </Grid>
-      </Grid>
-    </Box>
-     <h1 className='breadcrumbs'><Breadcrumbs /></h1>
-     </div>
-        );
-    }
+          {<img style={{ display: 'none' }} src={image} alt={imageText} />}
+          <Typography component="h1" variant="h2" className="main-head" align="center">{title}</Typography>
+        </Box>
+        <div className="breadcrumbs-wrapper">
+          <Breadcrumbs />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FullWidthBanner;
--- src/Layout/BreadCrumbs.jsx
+++ src/Layout/BreadCrumbs.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { Breadcrumbs } from '@material-ui/core';
 import { Link } from "react-router-dom";
 import withBreadcrumbs from "react-router-breadcrumbs-hoc";
+import '../Styles/breadcrumbs.scss';
 
 const PureBreadcrumbs = ({ breadcrumbs }) => {
-    return (
-
-        <Breadcrumbs>
-            {breadcrumbs.map(({ breadcrumb, match }, index) => (
-                <div key={match.url} >
-                    <Link to={match.url || ""}>{breadcrumb }</Link>
-                </div>
-            ))}
-        </Breadcrumbs>
-    )
+  return (
+    <Breadcrumbs separator="›">
+      {breadcrumbs.map(({ breadcrumb, match }, index) => (
+        <div key={match.url} >
+          <Link to={match.url || ""} key={index}>{breadcrumb}</Link>
+        </div>
+      ))}
+    </Breadcrumbs>
+  )
 };
 
 export default withBreadcrumbs()(PureBreadcrumbs); 
